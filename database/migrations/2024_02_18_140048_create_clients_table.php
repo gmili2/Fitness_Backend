f34858995d3cff13a14e93f0b37737ebde088a08
unknown <anouar.gmili@usmba.ac.ma>
2024-03-22 00:44:09 +0000

    first push

---
+++ database/migrations/2024_02_18_140048_create_clients_table.php
@@ -0,0 +1,38 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('email')->unique();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->integer('age');
+            $table->string('image_path')->nullable();
+            $table->string('phone_number');
+            $table->date('registration_date');
+            $table->date('expiration_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
