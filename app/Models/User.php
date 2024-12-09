668520084450511e038b3292badd31477401ee77
AnouarGmWobz <anouar.g@wobz.com>
2024-12-09 15:42:09 +0100

    weebhook test

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,8 +17,12 @@ class User extends Authenticatable implements JWTSubject
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
     ];
+
+
 
     /**
      * The attributes that should be hidden for arrays.
@@ -26,7 +30,8 @@ class User extends Authenticatable implements JWTSubject
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
     ];
 
     /**
